bae100218760104024ba3fd83dc3faddc9e7de82
Matthew Sherman <msherman0801@gmail.com>
2019-01-23 19:09:48 -0500

    done

---
+++ artist.rb
@@ -0,0 +1,17 @@
+require 'pry'
+class Artist < ActiveRecord::Base
+
+    # attr_accessor :name, :genre, :age, :hometown
+    # @@connect = ActiveRecord::Base.connection
+    # def initialize(attributes={})
+    #     attributes.each do |key, value|
+    #         self.send("#{key}=", value)
+    #     end
+    # end
+
+    # def save
+    #     sql = "INSERT INTO "
+    #     # @@connect.create("name" => @name, "genre" => @genre, "age" => @age, "hometown" => @hometown)
+    # end
+
+end
